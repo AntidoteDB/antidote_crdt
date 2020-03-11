deabc28b5d2af5f09a5cddfb78dc0708c915abbf
Pascal Dierich <pascal@pascaldierich.com>
2020-03-11 13:55:53 +0100

    Remove generic flag type, redefine flag_ew type
    Removes the flag type in `antidote_crdt_flag_helper`
    and changes the `flag_ew` type to `antidote_crdt_flag_helper:tokens()`.
    
    To do this the `tokens` type needs to be exported
    in `antidote_crdt_flag_helper`.
    
    Creates more consistency with the `flag_dw` type.

--- src/antidote_crdt_flag_ew.erl
+++ src/antidote_crdt_flag_ew.erl
@@ -54,7 +54,7 @@
 -define(TAG, 77).
 -define(V1_VERS, 1).
 
--type flag_ew() :: antidote_crdt_flag_helper:flag().
+-type flag_ew() :: antidote_crdt_flag_helper:tokens().
 
 %% SeenTokens, NewTokens
 -type downstream_op() :: {antidote_crdt_flag_helper:tokens(), antidote_crdt_flag_helper:tokens()}.
